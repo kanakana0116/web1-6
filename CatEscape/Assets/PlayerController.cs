7cf546b7699e2a999a039e9d0a8fd474954d572a
Unknown <kanakana0116@icloud.com>
2018-06-04 19:04:16 +0900

    猫のゲーム作りました

---
+++ CatEscape/Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+   
+    
+        public void LeftArrow() {
+            transform.Translate(-3, 0, 0);
+        }
+    public void ReftArrow() {
+            transform.Translate(3, 0, 0);
+        }
+    }
